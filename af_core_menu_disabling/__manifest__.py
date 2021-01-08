94f3dff71a6ddfce7250dfd2852658f8811a69f8
Fredrik Arvas <fredrik@arvas.se>
2021-01-08 08:32:49 +0100

    Updated manifest and removed websiteconfig

--- af_core_menu_disabling/__manifest__.py
+++ af_core_menu_disabling/__manifest__.py
@@ -3,11 +3,15 @@
 {
     'name': "Disable access for core menus",
     'summary': "Hide some specific menus for internal type of user.",
+    'description': "This module adds a access-group for standard-menus. If the user is not part of that group, the menu will be hidden.\n
+    The current hidden menues are: contacts, discuss, calendar. \n
+    v12.0.1.0.1 Added description and removed Website Config.", 
+    
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     'category': 'Tools',
-    'version': '12.0.1.0',
+    'version': '12.0.1.0.1',
     'depends': ['calendar','website','contacts'],
     'data': [
         'security/security_view.xml',
